cc56318ac1b4f8789383313fd0967fa2ce5e9bc4
Cavin <cavinmacwan1@gmail.com>
2025-02-25 11:48:48 +0530

    imp: portfolio details

--- pages/articles/index.vue
+++ pages/articles/index.vue
@@ -11,7 +11,7 @@
 
 <script setup>
 const description =
-  "All of my long-form thoughts on programming, user interfaces, product design, and more, collected in chronological order.";
+  "All of my long-form thoughts on programming, Jetpack Compose, product design, and more, collected in chronological order.";
 useSeoMeta({
   title: "Articles | Cavin Macwan",
   description,
