d14b57766c645de02454691625e314b8b75d446f
Cavin <cavinmacwan1@gmail.com>
2025-02-25 13:24:20 +0530

    Added GTag & SEO

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,9 @@ export default defineNuxtConfig({
     "@nuxt/image",
     "@nuxt/content",
     "@nuxthq/studio",
-    "@vueuse/nuxt"
+    "@vueuse/nuxt",
+    "@nuxtjs/seo",
+    "nuxt-gtag",
   ],
   ui: {
     icons: ["heroicons", "lucide"],
@@ -36,4 +38,18 @@ export default defineNuxtConfig({
       Inter: [400, 500, 600, 700, 800, 900],
     },
   },
+  site: {
+    url: 'https://cavinmacwan.com',
+    name: 'Cavin Macwan - Jetpack Compose, Flutter, Co-Founder',
+    description: 'I\'m Cavin Macwan, your friendly neighborhood software, product engineer, and designer. I specialize in building mobile apps using Jetpack Compose, Flutter, and other mobile technologies.',
+    defaultLocale: 'en',
+    
+  },
+  runtimeConfig: {
+    public: {
+      gtag: {
+        id: 'G-Q9P3KGT7EJ'
+      }
+    }
+  }
 });
