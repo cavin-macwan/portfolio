c7827a58e39f0d4bcbd37fef0501822440a382c9
Cavin <cavinmacwan1@gmail.com>
2025-04-06 14:04:14 +0530

    add structured data for Cavin Macwan in index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -9,3 +9,32 @@
     </div>
   </main>
 </template>
+
+<script setup lang="ts">
+useHead({
+  script: [
+    {
+      type: 'application/ld+json',
+      children: JSON.stringify({
+        "@context": "https://schema.org",
+        "@type": "Person",
+        "name": "Cavin Macwan",
+        "url": "https://cavinmacwan.com",
+        "image": "https://www.cavinmacwan.com/avatar.jpeg",
+        "sameAs": [
+          "https://twitter.com/cavin_1910",
+          "https://www.linkedin.com/in/cavin-macwan",
+          "https://github.com/cavin-macwan"
+        ],
+        "jobTitle": "Co-Founder",
+        "worksFor": {
+          "@type": "Organization",
+          "name": "Meticha"
+        },
+        "description": "Cavin Macwan is a mobile app developer specializing in Jetpack Compose, Flutter, and Kotlin. He is the Co-Founder of Meticha and shares insights for indie devs.",
+        "knowsAbout": ["Jetpack Compose", "Flutter", "Kotlin", "Android Development"]
+      })
+    }
+  ]
+})
+</script>
